ff9c6d99739a3a670786daa3a4e8c334d206f8a0
snr745 <snr745@gmail.com>
2021-04-23 16:48:09 +0530

    New_WebAoi_Project

---
+++ Controllers/DepartMentController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Web.Http;
+using System.Net.Http;
+using System.Net;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class DepartMentController:ApiController
+    {
+        public HttpResponseMessage Get()
+        {
+            string query = "select DepartmentId,DepartmentName from dbo.Department";
+            DataTable table = new DataTable();
+            using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["EmployeeAppDB"].ConnectionString))
+                using (var cmd=new SqlCommand(query,con))
+                using (var da=new SqlDataAdapter(cmd))
+            {
+                cmd.CommandType = CommandType.Text;
+                da.Fill(table);
+            }
+            return Request.CreateResponse(HttpStatusCode.OK, table);
+        }
+
+        public string Post(Department dep)
+        {
+            try
+            {
+                string query = @"insert into dbo.Department value
+('" + dep.DepartMentName + @"')
+";
+                DataTable table = new DataTable();
+                using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["EmployeeAppDB"].ConnectionString))
+                using (var cmd = new SqlCommand(query, con))
+                using (var da = new SqlDataAdapter(cmd))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    da.Fill(table);
+                }
+                return "Added Succesfully!!!";
+            }
+            catch (Exception ex)
+            {
+                return "Failed To Add!!!";
+            }
+        }
+        public string Put(Department dep)
+        {
+            try
+            {
+                string query = @"update   dbo.Department set DepartmentName='" + dep.DepartMentName + @"'
+ Where DepartMentId="+dep.DepartmentId+@"";
+                DataTable table = new DataTable();
+                using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["EmployeeAppDB"].ConnectionString))
+                using (var cmd = new SqlCommand(query, con))
+                using (var da = new SqlDataAdapter(cmd))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    da.Fill(table);
+                }
+                return "Updated Succesfully!!!";
+            }
+            catch (Exception ex)
+            {
+                return "Update Failed!!!";
+            }
+        }
+
+        public string Delete(int id)
+        {
+            try
+            {
+                string query = @"delete from   dbo.Department where DepartmentId="+id+@"";
+                DataTable table = new DataTable();
+                using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["EmployeeAppDB"].ConnectionString))
+                using (var cmd = new SqlCommand(query, con))
+                using (var da = new SqlDataAdapter(cmd))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    da.Fill(table);
+                }
+                return "Deleted Succesfully!!!";
+            }
+            catch (Exception ex)
+            {
+                return "Delete  Failed!!!";
+            }
+        }
+    }
+}
---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        // GET: Employee
+        public HttpResponseMessage Get()
+        {
+            string query = "select EmployeeId,EmployeeName,Department,DateofJoining,PhotoFileName from dbo.Employee";
+            DataTable table = new DataTable();
+            using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["EmployeeAppDB"].ConnectionString))
+            using (var cmd = new SqlCommand(query, con))
+            using (var da = new SqlDataAdapter(cmd))
+            {
+                cmd.CommandType = CommandType.Text;
+                da.Fill(table);
+            }
+            return Request.CreateResponse(HttpStatusCode.OK, table);
+        }
+
+        public string Post(Employee emp)
+        {
+            try
+            {
+                string query = @"insert into dbo.Employee values
+('" + emp.EmployeeName + @"','" + emp.Department + @"','" + emp.DateOfJoining + @"','" + emp.PhotoFileName + @"')";
+                DataTable table = new DataTable();
+                using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["EmployeeAppDB"].ConnectionString))
+                using (var cmd = new SqlCommand(query, con))
+                using (var da = new SqlDataAdapter(cmd))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    da.Fill(table);
+                }
+                return "Added Succesfully!!!";
+            }
+            catch (Exception ex)
+            {
+                return "Failed To Add!!!";
+            }
+        }
+        public string Put(Employee emp)
+        {
+            try
+            {
+                string query = @"update   dbo.Employee set 
+                          EmployeeName='" + emp.EmployeeName + @"'
+                          ,Department='" + emp.Department + @"'
+                          ,DateofJoining='" + emp.DateOfJoining + @"'
+                          ,PhotoFileName='" + emp.PhotoFileName + @"'
+                          Where EmployeeId=" + emp.EmployeeId + @"";
+                DataTable table = new DataTable();
+                using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["EmployeeAppDB"].ConnectionString))
+                using (var cmd = new SqlCommand(query, con))
+                using (var da = new SqlDataAdapter(cmd))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    da.Fill(table);
+                }
+                return "Updated Succesfully!!!";
+            }
+            catch (Exception ex)
+            {
+                return "Update Failed!!!";
+            }
+        }
+
+        [Route("api/Employee/SaveFile")]
+        public string SaveFile()
+        {
+            try
+            {
+                var HttpRequest = HttpContext.Current.Request;
+                var PostedFile = HttpRequest.Files[0];
+                string FileName = PostedFile.FileName;
+                var PhysicalPath = HttpContext.Current.Server.MapPath("~/Photos/" + FileName);
+                PostedFile.SaveAs(PhysicalPath);
+                return FileName;
+            }
+            catch (Exception Ex)
+            {
+                return "Save FIle Failed";
+            }
+        }
+
+        public string Delete(int id)
+        {
+            try
+            {
+                string query = @"delete from   dbo.Employee where EmployeeId=" + id + @"";
+                DataTable table = new DataTable();
+                using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["EmployeeAppDB"].ConnectionString))
+                using (var cmd = new SqlCommand(query, con))
+                using (var da = new SqlDataAdapter(cmd))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    da.Fill(table);
+                }
+                return "Deleted Succesfully!!!";
+            }
+            catch (Exception ex)
+            {
+                return "Delete  Failed!!!";
+            }
+        }
+    }
+}
